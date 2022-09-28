635ea134d277dd2802c1eec26c1e4603e1abc3b7
TilmeezUrRehmanBhatti <80107049+TilmeezUrRehmanBhatti@users.noreply.github.com>
2022-09-28 14:19:21 +0200

    Developing the DAO Interface and Implementation

---
+++ src/main/java/com/tilmeez/cruddemo/dao/EmployeeDAO.java
@@ -0,0 +1,11 @@
+package com.tilmeez.cruddemo.dao;
+
+import com.tilmeez.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+
+    public List<Employee> finalAll();
+
+}
