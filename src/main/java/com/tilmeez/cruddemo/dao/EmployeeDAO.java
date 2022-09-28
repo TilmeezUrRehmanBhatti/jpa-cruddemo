795f9024d1ceeaa6656f99f0d18179473d376fc5
TilmeezUrRehmanBhatti <80107049+TilmeezUrRehmanBhatti@users.noreply.github.com>
2022-09-28 14:46:15 +0200

    Additional CRUD Methods for DAO

--- src/main/java/com/tilmeez/cruddemo/dao/EmployeeDAO.java
+++ src/main/java/com/tilmeez/cruddemo/dao/EmployeeDAO.java
@@ -8,4 +8,10 @@ public interface EmployeeDAO {
 
     public List<Employee> finalAll();
 
+    public Employee findById(int theId);
+
+    public void save(Employee theEmployee);
+
+    public void deleteById(int theId);
+
 }
